5130c45ec6e4f88053c3d5d8bb2ab9e9388e8963
Ben Girard <benjamingirard2021@u,nothwestern.edu>
2019-01-25 12:40:32 -0600

    This is my commit message

--- student_code.py
+++ student_code.py
@@ -7,31 +7,43 @@ class KnowledgeBase(object):
     def __init__(self, facts=[], rules=[]):
         self.facts = facts
         self.rules = rules
-
+    
     def __repr__(self):
         return 'KnowledgeBase({!r}, {!r})'.format(self.facts, self.rules)
-
+    
     def __str__(self):
         string = "Knowledge Base: \n"
         string += "\n".join((str(fact) for fact in self.facts)) + "\n"
         string += "\n".join((str(rule) for rule in self.rules))
         return string
-
+    
+    
+    
+    
     def kb_assert(self, fact):
         """Assert a fact or rule into the KB
-
-        Args:
+            Args:
             fact (Fact or Rule): Fact or Rule we're asserting in the format produced by read.py
-        """
-        print("Asserting {!r}".format(fact))
+            """
+        if isinstance(fact, Fact):
+            self.facts.append(fact)
         
+        
+        print("Asserting {!r}".format(fact))
+    
     def kb_ask(self, fact):
         """Ask if a fact is in the KB
-
-        Args:
+            Args:
             fact (Fact) - Fact to be asked
-
-        Returns:
+            Returns:
             ListOfBindings|False - ListOfBindings if result found, False otherwise
-        """
-        print("Asking {!r}".format(fact))
+            """
+        results = ListOfBindings()
+        
+        if isinstance(fact, Fact):
+            for x in self.facts:
+                if isinstance(match(fact.statement, x.statement), Bindings) == True:
+                    results.add_bindings(match(fact.statement, x.statement))
+        return results
+
+    print("Asking {!r}".format(fact))
